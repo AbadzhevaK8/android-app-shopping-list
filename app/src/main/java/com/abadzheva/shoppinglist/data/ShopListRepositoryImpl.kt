cf8c651ba06893d644dba4b9bf11d804640b5a43
k8abadzheva <make.me.loco@gmail.com>
2024-08-02 00:50:04 +0300

    feat: Add ShopListRepositoryImpl
    Added ShopListRepositoryImpl that implements ShopListRepository interface.
    Also, added UNDEFINED_ID to ShopItem
     to make id optional.

---
+++ app/src/main/java/com/abadzheva/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,33 @@
+package com.abadzheva.shoppinglist.data
+
+import com.abadzheva.shoppinglist.domain.ShopItem
+import com.abadzheva.shoppinglist.domain.ShopListRepository
+
+object ShopListRepositoryImpl : ShopListRepository {
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItemId: Int) {
+        shopList.removeAt(shopItemId)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem =
+        shopList.find { it.id == shopItemId }
+            ?: throw RuntimeException("Element with id $shopItemId not found")
+
+    override fun getShopList(): List<ShopItem> = shopList.toList()
+}
