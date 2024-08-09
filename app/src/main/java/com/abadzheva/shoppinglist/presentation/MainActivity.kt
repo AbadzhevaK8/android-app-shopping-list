03ac42dfb4939562dc4191d647af37aa5ca93538
k8abadzheva <make.me.loco@gmail.com>
2024-08-10 01:29:14 +0300

    feat: Add navigation to ShopItemActivity
    Added the ability to navigate from MainActivity to ShopItemActivity to add or edit a shop item.
    
    - Added "
    Add" button to MainActivity.
    - Implemented navigation to ShopItemActivity when clicking on a list item or the "Add" button.
    - Updated ShopItemActivity to handle different screen modes (add or edit).
    - Updated the app icon.

--- app/src/main/java/com/abadzheva/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/abadzheva/shoppinglist/presentation/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.abadzheva.shoppinglist.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
@@ -29,6 +30,11 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
             shopListAdapter.submitList(it)
+        }
+        val buttonAddItem = findViewById<FloatingActionButton>(R.id.button_add_shop_item)
+        buttonAddItem.setOnClickListener {
+            val intent = ShopItemActivity.newIntentAddItem(this)
+            startActivity(intent)
         }
     }
 
@@ -78,6 +84,8 @@ class MainActivity : AppCompatActivity() {
     private fun setupClickListener() {
         shopListAdapter.onShopItemClickListener = {
             Log.d("MainActivity", it.toString())
+            val intent = ShopItemActivity.newIntentEditItem(this, it.id)
+            startActivity(intent)
         }
     }
 
--- app/src/main/java/com/abadzheva/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/abadzheva/shoppinglist/presentation/ShopItemActivity.kt
@@ -1,18 +1,18 @@
 package com.abadzheva.shoppinglist.presentation
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.abadzheva.shoppinglist.R
 
 class ShopItemActivity : AppCompatActivity() {
-    private lateinit var viewModel: ShopItemViewModel
-
+    //    private lateinit var viewModel: ShopItemViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_shop_item)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -20,6 +20,31 @@ class ShopItemActivity : AppCompatActivity() {
             insets
         }
         // ---------------------------
-//        viewModel.errorInputName.value = false
+        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
+        Log.d("ShopItemActivity", mode.toString())
+    }
+
+    companion object {
+        private const val EXTRA_SCREEN_MODE = "extra_mode"
+        private const val EXTRA_SHOP_ITEM_ID = "extra_shop_item_id"
+        private const val MODE_EDIT = "mode_edit"
+        private const val MODE_ADD = "mode_add"
+        private const val MODE_UNKNOWN = ""
+
+        fun newIntentAddItem(context: Context): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_ADD)
+            return intent
+        }
+
+        fun newIntentEditItem(
+            context: Context,
+            shopItemId: Int,
+        ): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(EXTRA_SCREEN_MODE, MODE_EDIT)
+            intent.putExtra(EXTRA_SHOP_ITEM_ID, shopItemId)
+            return intent
+        }
     }
 }
