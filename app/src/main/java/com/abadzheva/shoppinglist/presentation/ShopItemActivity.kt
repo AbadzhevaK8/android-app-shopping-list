60e4f73180c14d2e2c797aec6869b2d1d220ad7e
k8abadzheva <make.me.loco@gmail.com>
2024-08-15 22:05:11 +0300

    feat: Add landscape orientation support
    Added a new layout for landscape orientation.
    In landscape orientation, the app uses a two-pane layout with a list of
     items on the left and a details view on the right.
    In portrait orientation, the app uses a single-pane layout with a list of items and a separate activity for the details view.
    Removed unused code from ShopItemActivity.
    Added logs to ShopItemFragment.

--- app/src/main/java/com/abadzheva/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/abadzheva/shoppinglist/presentation/ShopItemActivity.kt
@@ -8,57 +8,19 @@ import com.abadzheva.shoppinglist.R
 import com.abadzheva.shoppinglist.domain.ShopItem
 
 class ShopItemActivity : AppCompatActivity() {
-    //    private lateinit var tilName: TextInputLayout
-//    private lateinit var etName: EditText
-//    private lateinit var tilCount: TextInputLayout
-//    private lateinit var etCount: EditText
-//    private lateinit var saveButton: Button
-//
-//    private lateinit var viewModel: ShopItemViewModel
-//
     private var screenMode = MODE_UNKNOWN
     private var shopItemId = ShopItem.UNDEFINED_ID
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.shop_item_container)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+
         // ---------------------------
         parseIntent()
-//        viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
-//        initViews()
-//        addTextChangeListeners()
-        launchRightMode()
-//        observeViewModel()
+        if (savedInstanceState == null) {
+            launchRightMode()
+        }
     }
-
-//    private fun observeViewModel() {
-//        viewModel.errorInputCount.observe(this) {
-//            val message =
-//                if (it) {
-//                    getString(R.string.error_input_count)
-//                } else {
-//                    null
-//                }
-//            tilCount.error = message
-//        }
-//        viewModel.errorInputName.observe(this) {
-//            val message =
-//                if (it) {
-//                    getString(R.string.error_input_name)
-//                } else {
-//                    null
-//                }
-//            tilName.error = message
-//        }
-//        viewModel.shouldCloseScreen.observe(this) {
-//            finish()
-//        }
-//    }
 
     private fun launchRightMode() {
         val fragment =
@@ -69,61 +31,10 @@ class ShopItemActivity : AppCompatActivity() {
             }
         supportFragmentManager
             .beginTransaction()
-            .add(R.id.shop_item_container, fragment)
+            .replace(R.id.shop_item_container, fragment)
             .commit()
     }
 
-    //
-//    private fun addTextChangeListeners() {
-//        etName.addTextChangedListener(
-//            object : TextWatcher {
-//                override fun beforeTextChanged(
-//                    s: CharSequence?,
-//                    start: Int,
-//                    count: Int,
-//                    after: Int,
-//                ) {
-//                }
-//
-//                override fun onTextChanged(
-//                    s: CharSequence?,
-//                    start: Int,
-//                    before: Int,
-//                    count: Int,
-//                ) {
-//                    viewModel.resetErrorInputName()
-//                }
-//
-//                override fun afterTextChanged(s: Editable?) {
-//                }
-//            },
-//        )
-//
-//        etCount.addTextChangedListener(
-//            object : TextWatcher {
-//                override fun beforeTextChanged(
-//                    s: CharSequence?,
-//                    start: Int,
-//                    count: Int,
-//                    after: Int,
-//                ) {
-//                }
-//
-//                override fun onTextChanged(
-//                    s: CharSequence?,
-//                    start: Int,
-//                    before: Int,
-//                    count: Int,
-//                ) {
-//                    viewModel.resetErrorInputCount()
-//                }
-//
-//                override fun afterTextChanged(s: Editable?) {
-//                }
-//            },
-//        )
-//    }
-//
     private fun parseIntent() {
         if (!intent.hasExtra(EXTRA_SCREEN_MODE)) {
             throw RuntimeException("Param screen mode is absent")
@@ -140,31 +51,6 @@ class ShopItemActivity : AppCompatActivity() {
             shopItemId = intent.getIntExtra(EXTRA_SHOP_ITEM_ID, ShopItem.UNDEFINED_ID)
         }
     }
-//
-//    private fun launchEditMode() {
-//        viewModel.getShopItem(shopItemId)
-//        viewModel.shopItem.observe(this) {
-//            etName.setText(it.name)
-//            etCount.setText(it.count.toString())
-//        }
-//        saveButton.setOnClickListener {
-//            viewModel.editShopItem(etName.text?.toString(), etCount.text?.toString())
-//        }
-//    }
-//
-//    private fun launchAddMode() {
-//        saveButton.setOnClickListener {
-//            viewModel.addShopItem(etName.text?.toString(), etCount.text?.toString())
-//        }
-//    }
-//
-//    private fun initViews() {
-//        tilName = findViewById(R.id.til_name)
-//        etName = findViewById(R.id.et_name)
-//        tilCount = findViewById(R.id.til_count)
-//        etCount = findViewById(R.id.et_count)
-//        saveButton = findViewById(R.id.save_button)
-//    }
 
     companion object {
         private const val EXTRA_SCREEN_MODE = "extra_mode"
