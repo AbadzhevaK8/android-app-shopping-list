7fd635644ec5e56b1fcca79762a7221255fadb0c
k8abadzheva <make.me.loco@gmail.com>
2024-09-14 23:01:33 +0300

    feat: Add database
    Added database with dao, entity and mapper.
    Updated ShopListRepositoryImpl to use the database.
    Removed test data.

---
+++ app/src/main/java/com/abadzheva/shoppinglist/data/ShopListMapper.kt
@@ -0,0 +1,26 @@
+package com.abadzheva.shoppinglist.data
+
+import com.abadzheva.shoppinglist.domain.ShopItem
+
+class ShopListMapper {
+    fun mapEntityToDbModel(shopItem: ShopItem): ShopItemDbModel =
+        ShopItemDbModel(
+            id = shopItem.id,
+            name = shopItem.name,
+            count = shopItem.count,
+            enabled = shopItem.enabled,
+        )
+
+    fun mapDbModelToEntity(shopItemDbModel: ShopItemDbModel) =
+        ShopItem(
+            id = shopItemDbModel.id,
+            name = shopItemDbModel.name,
+            count = shopItemDbModel.count,
+            enabled = shopItemDbModel.enabled,
+        )
+
+    fun mapListDbModelToListEntity(list: List<ShopItemDbModel>) =
+        list.map {
+            mapDbModelToEntity(it)
+        }
+}
