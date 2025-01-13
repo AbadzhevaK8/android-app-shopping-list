6054307489718ee764900005a7aeb9ea4bca455b
k8abadzheva <make.me.loco@gmail.com>
2025-01-13 23:30:37 +0300

    feat: Add UriMatcher to ShopListProvider
    Added UriMatcher to ShopListProvider to handle different URIs.
    Added a constant for the GET_SHOP_ITEMS_QUERY code.
    Log the URI and the match code in the query method.

--- app/src/main/java/com/abadzheva/shoppinglist/data/ShopListProvider.kt
+++ app/src/main/java/com/abadzheva/shoppinglist/data/ShopListProvider.kt
@@ -2,21 +2,36 @@ package com.abadzheva.shoppinglist.data
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
 import android.util.Log
 
 class ShopListProvider : ContentProvider() {
+    private val uriMatcher =
+        UriMatcher(UriMatcher.NO_MATCH).apply {
+            addURI(
+                "com.abadzheva.shoppinglist",
+                "shop_items",
+                GET_SHOP_ITEMS_QUERY,
+            )
+        }
+
     override fun onCreate(): Boolean = true
 
     override fun query(
-        p0: Uri,
+        uri: Uri,
         p1: Array<out String?>?,
         p2: String?,
         p3: Array<out String?>?,
         p4: String?,
     ): Cursor? {
-        Log.d("ShopListProvider", "query: $p0")
+        val code = uriMatcher.match(uri)
+        when (code) {
+            GET_SHOP_ITEMS_QUERY -> {
+            }
+        }
+        Log.d("ShopListProvider", "query: $uri code $code")
         return null
     }
 
@@ -46,5 +61,9 @@ class ShopListProvider : ContentProvider() {
         p3: Array<out String?>?,
     ): Int {
         TODO("Not yet implemented")
+    }
+
+    companion object {
+        private const val GET_SHOP_ITEMS_QUERY = 100
     }
 }
